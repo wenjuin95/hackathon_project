94020b19c3940c2a09cdc1e05ba17025014af22c
nelsonwm45 <nelsonwm45@gmail.com>
2024-07-28 05:32:03 +0800

    commit before Jackie test

--- frontend/js/fileUpload.js
+++ frontend/js/fileUpload.js
@@ -1,5 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { useState, useEffect } from 'react'
+import { 
+  ConnectButton,
+  // ConnectModal, 
+  createNetworkConfig, 
+  // SuiClientProvider, 
+  // WalletProvider 
+} from '@mysten/dapp-kit';
+import { useCurrentAccount } from '@mysten/dapp-kit';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
+import '@mysten/dapp-kit/dist/index.css';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCkeSacH6emGscYhdK8ru1q_n4qFLaVLY",
@@ -11,22 +25,38 @@ const firebaseConfig = {
   measurementId: "G-WB2GL49FTK"
 };
 
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: getFullnodeUrl('testnet') },
+  devnet: { url: getFullnodeUrl('devnet') },
+})
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
+
 
 // Select file input and progress bar elements
 const fileInput = document.getElementById('upload-button');
 const progressBar = document.querySelector('.progress');
 const uploadPercentage = document.querySelector('.uploadPercentage');
+await showHideUpload();
 
 // Listen for file selection
 fileInput.addEventListener('change', async (event) => {
   const file = event.target.files[0];
-  uploadFile(file);                       
+
+  uploadFile(file);
   console.log("File selected:", file.name); // Debug 
 });
 
+async function ObtainBalance() {
+  const account = useCurrentAccount();
+  const suiClient = new SuiClient({ url: getFullnodeUrl('testnet') });
+  const balance = await suiClient.getBalance({ owner: account.address });
+
+  return (
+    balance
+  );
+}
 
 function uploadFile(file) {
   const storageRef = ref(storage, 'uploads/' + file.name);
@@ -64,3 +94,15 @@ document.getElementById('upload-button').addEventListener('click', () => {
   fileInput.click(); // Trigger file input dialog
   console.log("File input dialog triggered"); // Debug log
 });
+
+async function showHideUpload() {
+  const serviceCharge = 0;
+  var balance = await ObtainBalance();
+
+  if (balance >= serviceCharge) {
+    uploadButton.hidden = false;
+  }
+  else {
+    console.log("hahahahaha");
+  }
+}
