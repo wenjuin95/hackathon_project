4a61aa41d9ba42a68d1cf1a02d7bcf8cd741cfc9
nelsonwm45 <nelsonwm45@gmail.com>
2024-07-28 08:08:31 +0800

    Modify stuff

--- frontend/js/fileUpload.js
+++ frontend/js/fileUpload.js
@@ -23,7 +23,7 @@ const storage = getStorage(app);
 const fileInput = document.getElementById('upload-button');
 const progressBar = document.querySelector('.progress');
 const uploadPercentage = document.querySelector('.uploadPercentage');
-const serviceCharge = 0;
+const serviceCharge = 1;
 setTimeout(showHideUpload, 1000);
 
 // Listen for file selection
